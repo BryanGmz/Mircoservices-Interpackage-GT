d848ac13f01b08dc04ddbb88b3c3a51ed73f7b02
Luis <louiscrack50@gmail.com>
2022-05-04 23:08:59 -0600

    adding post functionality to queue controller

--- src/main/java/com/gt/interpackage/administration/controller/QueueController.java
+++ src/main/java/com/gt/interpackage/administration/controller/QueueController.java
@@ -1,11 +1,16 @@
 package com.gt.interpackage.administration.controller;
 
+import com.gt.interpackage.administration.model.Queue;
 import com.gt.interpackage.administration.handlers.QueueHandler;
+import com.gt.interpackage.administration.service.QueueService;
 import com.gt.interpackage.administration.source.Constants;
+import java.net.URI;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +22,9 @@ public class QueueController {
 
     @Autowired
     private QueueHandler queueHandler;
+    
+    @Autowired
+    private QueueService _queueService;
 
     @GetMapping("/")
     public ResponseEntity<?> prueba(){
@@ -28,5 +36,19 @@ public class QueueController {
         }
     }
 
+    @PostMapping("/")
+    public ResponseEntity<Queue> addQueue(@RequestBody Queue queue) {
+        try { 
+            Queue savedQueue = _queueService.save(queue);
+            return ResponseEntity.created(
+                    new URI("/queue/" + savedQueue.getQueue()))
+                    .body(savedQueue);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build(); // 400 Bad Request
+        }
+    } 
+    
+    
+    
 
 }
